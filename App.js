23cc67f3b452a147add36151861cafbfc5727bc5
AytacSahin <haytacsahin@gmail.com>
2023-11-06 16:44:57 +0300

    first login screen & music and audio files

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Video } from 'expo-av';
+import AnimatedText from './components/animatedtext';
+import Music from './components/music';
 
 export default function App() {
+
+  Music(); // müzik componenti çalıştırıldı.
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Mobile App</Text>
+      <Video
+        source={require('./assets/Ocean.mp4')}
+        rate={1.0}
+        volume={1.0}
+        isMuted={true}
+        resizeMode="cover"
+        shouldPlay={true}
+        isLooping={true}
+        useNativeControls={false}
+        style={StyleSheet.absoluteFillObject} // Videoyu ekranı kaplayacak şekilde ayarla
+      />
+      <View style={styles.overlay}>
+        <AnimatedText text='Mobile App' style={styles.text}>Mobile App</AnimatedText>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,12 +33,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: 'white',
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
     alignItems: 'center',
-    justifyContent: 'center',
   },
   text: {
-    fontSize: 70,
-    color: 'white'
+    fontSize: 28,
+    marginTop: 80,
   },
+  
 });
